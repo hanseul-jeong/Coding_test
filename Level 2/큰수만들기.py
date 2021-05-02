67da7569442c1eeed15fc451e3ef75363d048796
Han-Seul Jeong <hanseul_j@korea.ac.kr>
2021-05-02 18:15:09 +0900

    Update 큰수만들기.py

--- Level 2/큰수만들기.py
+++ Level 2/큰수만들기.py
@@ -1,3 +1,24 @@
+"""
+어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+
+예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
+
+문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
+
+제한 조건
+number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
+k는 1 이상 number의 자릿수 미만인 자연수입니다.
+"""
+# 21.05.02 version 아직.. 시간초과로 33점
+
+def solution(number, k):
+    max_n = '0'
+    numbers = [[n, i] for i, n in enumerate(number)]
+    from itertools import combinations
+    cands = list(combinations(numbers, len(numbers) - k))
+    cands = list(set([''.join([y[0] for y in cand]) for cand in cands]))
+    return str(max(map(int, cands)))
+
 # test 10 제한시간 초과 ###########################################
 # def get_max(str):
 #     max_idx = 0
@@ -33,14 +54,4 @@
 #     candidates = list(combinations([number[i] for i in range(len(number))],len(number)-k))
 #     candidates = sorted([int("".join(c)) for c in candidates ], reverse=True)
 #     return str(candidates[0])
-"""
-어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
 
-예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
-
-문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
-
-제한 조건
-number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
-k는 1 이상 number의 자릿수 미만인 자연수입니다.
-"""
