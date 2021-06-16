71a2fab7053ad0e6101da2b0cb54581f11f5e232
Han-Seul Jeong <hanseul_j@korea.ac.kr>
2021-06-16 13:22:22 +0900

    Create 15_3Sum.py
    incomplete

---
+++ LeetCode/15_3Sum.py
@@ -0,0 +1,38 @@
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+ 
+
+Constraints:
+
+0 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+'''
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # nums.sort()
+        results = []
+        for i in range(len(nums)-2):
+            for j in range(i+1, len(nums)-1):
+                for k in range(j+1, len(nums)):
+                    cand = [nums[i], nums[j], nums[k]]
+                    if sum(cand) == 0 and sorted(cand) not in results:
+                        results.append(sorted(cand))
+        return results
