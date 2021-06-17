27a0dba7e1e901aed3c211040bdc7ced51ea1277
Han-Seul Jeong <hanseul_j@korea.ac.kr>
2021-06-17 11:07:58 +0900

    Update 21_Merge-Two-Sorted-Lists.py
    add the solution of book

--- LeetCode/21_Merge-Two-Sorted-Lists.py
+++ LeetCode/21_Merge-Two-Sorted-Lists.py
@@ -32,6 +32,13 @@ Both l1 and l2 are sorted in non-decreasing order.
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if (not l1) or (not l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+    
+    
+        # previous
         # check empty list
         if not l1 or not l2:
             return l1 if not l2 else l2
