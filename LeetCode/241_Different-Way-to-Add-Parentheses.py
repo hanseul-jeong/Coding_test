d64a4e21d66c3c45fea7fd87336f54022269f735
Han-Seul Jeong <hanseul_j@korea.ac.kr>
2021-07-28 16:25:34 +0900

    Update 241_Different-Way-to-Add-Parentheses.py

--- LeetCode/241_Different-Way-to-Add-Parentheses.py
+++ LeetCode/241_Different-Way-to-Add-Parentheses.py
@@ -28,6 +28,62 @@ Constraints:
 expression consists of digits and the operator '+', '-', and '*'.
 '''
 
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        def cal(cands):
+            results = []
+            for cand in cands:
+                stack = []
+                for c in cand:
+                    if c == ')':
+                        q = deque([])
+                        while stack:
+                            s = stack.pop()
+                            if s == '(': break
+                            q.appendleft(s)
+                        x,y,op = "", "", None
+                        while q:
+                            tmp = q.popleft()
+                            if tmp not in ['*','+','-']:
+                                x += tmp
+                            else:
+                                op = tmp
+                                break
+                        x, y = int(x), int(''.join(q))
+                        if op == '-':
+                            stack.append(str(x-y))
+                        elif op == '+':
+                            stack.append(str(x+y))
+                        else:
+                            stack.append(str(x*y))
+                    else:
+                        stack.append(c)
+                results.append(int(stack[0]))
+            return results
+        results = set([])
+        exps = []
+        tmp = expression[0]
+        for e in expression[1:]:
+            if e not in ['+','-','*']:
+                tmp += e
+            else:
+                exps.extend([tmp, e])
+                tmp = ""
+        exps.append(tmp)
+        expression = exps
+        cnt = (len(expression) - 1) // 2  # # of parentheses
+        if len(expression) == 1: return [int(expression[0])]
+        queue = deque([[expression, cnt]])
+        while queue:
+            left, cnt = queue.popleft()
+            if cnt == 0:
+                results.add(left[0])
+                continue
+            for idx in range(0, len(left)-1, 2):
+                queue.append([left[:idx] + ['(' + ''.join(left[idx:idx+3]) + ')'] + left[idx+3:], cnt-1])
+        return cal(results)
+
+
 # It could't remove overlapped case
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
